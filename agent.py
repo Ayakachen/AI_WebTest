81efa7af9975c9854df20a2973ff177cf08ea1a7
Ayakachen <ayaka_chan@163.com>
2025-09-01 16:49:51 +0800

    update

--- agent.py
+++ agent.py
@@ -1,20 +1,52 @@
 import asyncio
+
 # from langchain_openai import ChatOpenAI
 from browser_use import ChatGoogle
 from browser_use import Agent
+from pydantic import BaseModel
+
 llm = ChatGoogle(
     # base_url= "https://generativelanguage.googleapis.com/v1beta/",
     model="gemini-2.5-flash",
-    api_key= "AIzaSyCQswJ8iMgcS4xcPSeSWzEHA7jRZ-wMD0Y"
+    api_key="AIzaSyCQswJ8iMgcS4xcPSeSWzEHA7jRZ-wMD0Y",
 )
 extend_system_message = """
-记住最重要的规则:
-1、执行搜索任务时，优先打开 https://www.bing.com/?mkt=zh-CN 进行搜索。
-2、最后的输出结果，要用中文回答用户的问题。
-"""
+    1、执行搜索任务时，优先打开 https://www.baidu.com/ 进行搜索。
+	2、最后的输出结果，要用中文输出结果。
+	"""
+
+
+class Post(BaseModel):
+    post_title: str
+    post_url: str
+    num_comments: int
+    hours_since_post: int
+
+
+class Posts(BaseModel):
+    posts: list[Post]
+
+
 async def main():
-    task_baidu = "Search for today's weather"
-    agent = Agent(task=task_baidu, llm=llm, message_context=extend_system_message)
-    result = await agent.run()
-    print(result)
+    task_baidu = "搜索今天的天气"
+    agent = Agent(
+        task=task_baidu,
+        llm=llm,
+        extend_system_message=extend_system_message,
+        output_model_schema=Posts,
+    )
+    history = await agent.run()
+    result = history.final_result()
+    if result:
+        parsed: Posts = Posts.model_validate_json(result)
+        for post in parsed.posts:
+            print("\n--------------------------------")
+            print(f"Title:            {post.post_title}")
+            print(f"URL:              {post.post_url}")
+            print(f"Comments:         {post.num_comments}")
+            print(f"Hours since post: {post.hours_since_post}")
+    else:
+        print("No result")
+
+
 asyncio.run(main())
---
+++ agent_stock.py
@@ -0,0 +1,55 @@
+import asyncio
+
+# from langchain_openai import ChatOpenAI
+from browser_use import ChatGoogle
+from browser_use import Agent
+from pydantic import BaseModel
+
+llm = ChatGoogle(
+    # base_url= "https://generativelanguage.googleapis.com/v1beta/",
+    model="gemini-2.5-flash",
+    api_key="AIzaSyCQswJ8iMgcS4xcPSeSWzEHA7jRZ-wMD0Y",
+)
+extend_system_message = """
+    1、执行搜索任务时，优先打开 https://gushitong.baidu.com/ 进行搜索。
+    2、查看概览的“五日”股票趋势图，获取趋势图上每一个时间点的所有取值
+    3、总结每日的开盘价、收盘价、最高价、最低价、涨跌幅。
+	4、最后的输出结果，要用中文和表格格式输出结果。
+	"""
+
+
+class Post(BaseModel):
+    post_title: str
+    post_url: str
+    num_comments: int
+    hours_since_post: int
+
+
+class Posts(BaseModel):
+    posts: list[Post]
+
+
+async def main():
+    task_baidu = "搜索深信服的股票情况"
+    agent = Agent(
+        task=task_baidu,
+        llm=llm,
+        extend_system_message=extend_system_message,
+        # output_model_schema=Posts,
+    )
+    history = await agent.run()
+    result = history.final_result()
+    if result:
+        # parsed: Posts = Posts.model_validate_json(result)
+        # for post in parsed.posts:
+        #     print("\n--------------------------------")
+        #     print(f"Title:            {post.post_title}")
+        #     print(f"URL:              {post.post_url}")
+        #     print(f"Comments:         {post.num_comments}")
+        #     print(f"Hours since post: {post.hours_since_post}")
+        print(result)
+    else:
+        print("No result")
+
+
+asyncio.run(main())
